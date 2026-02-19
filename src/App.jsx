f55891fc3a05b9c84180f6cc38f40cad89b80d6d
vishal kumar <vishaljpkumar4970@gmail.com>
2026-02-19 11:46:08 +0530

    new commit page create contact us

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,7 @@ import { AnimatePresence, motion } from 'framer-motion'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import AOS from "aos";
-import "aos/dist/aos.css";
-
-
-
-
+import "aos/dist/aos.css"
 import Home from './pages/Home'
 import Results from './pages/Results'
 import SeatSelector from './pages/SeatSelector'
